c96914344cf074c92ba36d83bed2be83f59978e9
heymeworld <com01@knou.ac.kr>
2024-10-25 21:36:15 +0900

    20241025 MyVectorTest Q2-5 Finished

--- 20241024/src/MyVector.java
+++ 20241024/src/MyVector.java
@@ -1,28 +1,31 @@
+import java.util.Optional;
+
 public class MyVector {
     // Q2-1. Object 배열을 인스턴스 변수 정의 ############################################################################
-    Object[] objArr = new Object[3];
-//    Object[] objArr = {new Object(), new Object(), new Object()};
+    private Object[] objArr;
+//    Object[] objArr = {new MyVector(), new MyVector(), new MyVector()};
 
     public Object[] getObjArr() { return objArr; }
 
-    public Object[] setObjArr(int n) {
-        this.objArr = new Object[n];
-        return this.objArr;
-    }
+//    생각해보니, 생성자 있어서 필요없음
+//    public Object[] setObjArr() {
+//        this.objArr = new MyVector[capacity];
+//        return this.objArr;
+//    }
 
     // Q2-2. MyVector 기본 생성자, 매개변수 생성자 정의 ###################################################################
-    int capacity; // iv는 자동 초기화 0;
+    private int capacity; // iv는 자동 초기화 0;
 
     MyVector() {
         this(16);
     }
 
     MyVector(int capacity) {
-        this.capacity = capacity;
+        this.objArr = new MyVector[capacity];
     }
 
     // Q2-3. 객체 배열의 size 와 capacity ##############################################################################
-    int size;
+    private int size;
 
     public int size() {
         for (int i=0; i<objArr.length; i++) {
@@ -37,14 +40,40 @@ public class MyVector {
     }
 
     public boolean isEmpty() {
-        boolean result = false;
-
         for (int i=0; i<objArr.length; i++) {
-            if (objArr[i] == null)
-                result = true;
+            if (objArr[i] != null)
+                return false;
 //            else result = false;
         }
-        return result;
+        return true;
     }
-}
+
+    // Q2-4. 객체 배열 objArr 에 객체를 추가하는 메서드 #####################################################################
+    public void add(MyVector v) {
+        for (int i=0; i<objArr.length; i++) {
+            if (objArr[i] == null) {
+                objArr[i] = v;
+                System.out.println("An obj is added");
+                break;
+            }
+            System.out.println("Object[] objArr is already full.");
+        }
+    }
+
+    // Q2-5. 객체 배열 objArr 에 저장된 객체를 반환하는 메서드 ##############################################################
+    public Optional<MyVector> get(int index) {
+        MyVector v = getNaked(index); // getNaked()는 null 을 반환할 수 있음
+        return Optional.ofNullable(v); // Optional 로 감싸서 최종 반환
+    }
+
+    private MyVector getNaked(int index) { return (MyVector) objArr[index]; }
+
+    // Q2-6. 객체 배열 objArr 에 저장된 모든 객체를 문자열로 이어서 반환하도록 toString()을 오버라이딩 ###########################
+    @Override  // 1.선언부 똑같게 2.접근 제어자 좁게X 3.예외 많게X
+    public String toString() {
+        return "Object[" + capacity + "]";
+    }
+
+
+} // class 끝.
 
--- 20241024/test/MyVectorTest.java
+++ 20241024/test/MyVectorTest.java
@@ -1,39 +1,71 @@
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MyVectorTest {
     // Object 배열 테스트
-    @Test public void objTest() {
+    @Test public void Test2_1() {
         MyVector v = new MyVector();
-        Object[] objArr = v.getObjArr();
-        assertTrue(objArr.length==3); }
+        assertNull(v.getObjArr());
+    }
     
     // MyVector 클래스 생성자 테스트
     @Test
-    public void getCapacity() {
+    public void Test2_2() {
         MyVector v1 = new MyVector();
-        assertTrue(v1.capacity==16);
+        Object[] v1Arr = v1.getObjArr();
+        assertTrue(v1Arr.length==16);
 
 
         MyVector v2 = new MyVector(20);
-        assertTrue(v2.capacity==20);
+        Object[] v2Arr = v2.getObjArr();
+        assertTrue(v2.capacity()==20);
     }
     
     // size(), capacity() 테스트
     @Test
-    public void getObjSize() {
-        MyVector v = new MyVector();
-        assertTrue(v.size()==0);
-        assertTrue(v.capacity()==3);
-        assertTrue(v.isEmpty());
+    public void Test2_3() {
+        MyVector v1 = new MyVector();
+        assertTrue(v1.size()==0);
+        assertTrue(v1.capacity()==16);
+        assertTrue(v1.isEmpty());
 
-        v.setObjArr(5);
-        assertTrue(v.capacity()==5);
+        MyVector v2 = new MyVector(5);
+        assertTrue(v2.capacity()==5);
+    }
+    
+    // add(Object obj) 테스트
+    @Test
+    public void Test2_4() {
+        MyVector v = new MyVector();
+        v.add(new MyVector());  // new Object()는 왜 안 되지? -> ArrayStoreException; 생성자 MyVector(int capacity) 때문에
+        assertTrue(v.size()==1);
+    }
+
+    // Object get(int index) 테스트
+    @Test
+    public void Test2_5() {
+        MyVector v1 = new MyVector();
+        v1.add(new MyVector());
+        assertNotNull(v1.get(0));
+
+        assertNotNull(v1.get(1));
+        System.out.println("해당 인덱스는 " + v1.get(1) + " 상태 입니다.");
+
+         Optional<MyVector> opt = v1.get(1);
+         // isPresent() : Optional 객체가 값을 가지고 있다면 true, 값이 없다면 false 리턴
+         assertFalse(opt.isPresent());  // opt 대신 v1.get(1) 써도 됨
+         // message : true 이면 출력X, false 면 메세지 출력됨 (AssertionFailedError: 해당 index에는 객체가 없습니다. ==>)
+         //assertTrue(opt.isPresent(), "해당 index에는 객체가 없습니다.");
     }
 
 //    @Test
-//    public void test() {
+//    public void Test2_6() {
 //        MyVector v = new MyVector();
-//        assertTrue(v.setObjArr(5)==5);
+//        v.add();
 //    }
-}
+
+
+} // class 끝.
